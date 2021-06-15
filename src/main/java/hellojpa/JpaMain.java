ae1ee59f76f096e24008ba16fdbadef6cbdb8ff8
Oh <oss0202@naver.com>
2021-06-15 12:18:08 +0900

    Entity persist.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -13,11 +14,21 @@ public class JpaMain {
         // 2. EntityManager 생성
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        //transaction 처리가 반드시 있어야 한다.
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
         // 3. 실제 code 작성
-        // ...
+        Member member = new Member();
+
+        member.setId(1L);
+        member.setName("HelloA");
+
+        entityManager.persist(member);
+
+        tx.commit();
 
         entityManager.close();
-
         entityManagerFactory.close();;
     }
 }
