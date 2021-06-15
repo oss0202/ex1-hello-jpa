36344e3e86445e83786b8f9ee220bc8d7a803794
Oh <oss0202@naver.com>
2021-06-15 12:33:32 +0900

    JPA TEST STANDARD코드추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,6 +9,7 @@ public class JpaMain {
     public static void main(String[] args) {
         
         // 1. EntityManagerFactory 생성(persistence.xml name 매칭)
+        // application loading 시점에 DB당 딱 하나만 생성되어야 한다.
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
         
         // 2. EntityManager 생성
@@ -21,14 +22,16 @@ public class JpaMain {
         // 3. 실제 code 작성
         Member member = new Member();
 
-        member.setId(1L);
-        member.setName("HelloA");
+        member.setId(2L);
+        member.setName("HelloB");
 
         entityManager.persist(member);
 
         tx.commit();
 
         entityManager.close();
+        //WAS 가 종료되는 시점에 EntityManagerFactory 를 닫는다
+        // -> 내부적으로 Connection pooling에 대한 Resource가 Release된다.
         entityManagerFactory.close();;
     }
 }
---
+++ src/main/java/hellojpa/JpaMainStandard.java
@@ -0,0 +1,32 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMainStandard {
+    public static void main(String[] args) {
+        
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        try {
+            Member member = new Member();
+            member.setId(3L);
+            member.setName("HelloC");
+
+            entityManager.persist(member);
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();;
+    }
+}
