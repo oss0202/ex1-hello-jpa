1f1b2f26357a912bc6c8ee17b1039b61ee809612
Oh <oss0202@naver.com>
2021-07-23 18:10:29 +0900

    상속관계 매핑 - 구현 클래스마다 테이블 전략

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -8,11 +8,9 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Entity
-//@Inheritance(strategy = InheritanceType.JOINED) // 조인 전략
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일테이블 전략
-//@DiscriminatorColumn // 단일테이블 전략은 어노테이션이 없어도 자동으로 DTYPE이 생성된다.( 한테이블에 들어있어서 이게 어떤 테이블에서 인입된 것인지 모르기 때문이다.)
-// 있는게 좋다. 없어도 물론 되긴 하지만, 없을 경우 상속받는 테이블들 중에서 어떤 테이블에 매핑되는지 모른다.
-public class Item {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 구현 클래스마다 테이블 전략
+@DiscriminatorColumn
+public abstract class Item {// Item만 독단적으로 사용하지 않기 위해서 추상클래스로 선언
     @Id @GeneratedValue
     private Long id;
 
