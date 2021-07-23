986f5a1713099ff5eb4fd830e2771582ca2786be
Oh <oss0202@naver.com>
2021-07-23 18:11:00 +0900

    상속관계 매핑 - 구현 클래스마다 테이블 전략

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Setter
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 구현 클래스마다 테이블 전략
-@DiscriminatorColumn
+@DiscriminatorColumn// 테이블 자체가 다 다르기 때문에 의미가 없는 어노테이션이다.
 public abstract class Item {// Item만 독단적으로 사용하지 않기 위해서 추상클래스로 선언
     @Id @GeneratedValue
     private Long id;
